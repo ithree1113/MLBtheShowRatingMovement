8a0a11e1a4f9e3842f7bb7d0ccb1ae4ab9eef615
Eddie <Eddie_Cheng@pxmart.comtw>
2023-09-12 16:30:23 +0800

    Add ReseltViewController

---
+++ MLBtheShowRatingMovement/MLBtheShowRatingMovement/View/ReseltViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ReseltViewController.swift
+//  MLBtheShowRatingMovement
+//
+//  Created by ithree1113 on 2023/9/12.
+//
+
+import UIKit
+
+class ReseltViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
